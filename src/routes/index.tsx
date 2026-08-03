57eda743d047cabfa543e553374740df551f68cb
Karthikeyan Shankar <karthikeyanshankar8@gmail.com>
2026-08-03 16:09:22 +0530

    Fix target cursor off-center issue and expand target selectors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -45,7 +45,7 @@ function Index() {
       <div className="aurora-drift pointer-events-none absolute inset-0 -z-30" />
       <div className="starfield pointer-events-none absolute inset-0 -z-20" />
       <div className="grid-circuit pointer-events-none absolute inset-0 -z-10" />
-      <TargetCursor cursorColor="#ffffff" cursorColorOnTarget="#ffffff" />
+      <TargetCursor cursorColor="#ffffff" cursorColorOnTarget="#ffffff" targetSelector=".cursor-target, button, a" />
 
 
       {/* Hero */}
